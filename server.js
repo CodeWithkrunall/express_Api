bb283f67796d3731b3247f6c856c02fac5a78493
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-03-15 11:07:57 +0530

    save

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+//import express
 const express = require ('express');
-
+//creating an application
 const app = express();
 const port = 3000;
 
-
+//define routers
 app.get('/', (req, res)=>{
     res.send('Hello Word From express');
 });
+
+app.get('/web', (req, res)=>{
+    res.send('krunal is webdeveloper');
+});
+
+
  
+//start the server
 app.listen(port, () =>{
     console.log(`Exampleapp listening at http://localhost${port}`);
 })
